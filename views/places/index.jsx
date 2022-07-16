225a666047cede45fd4c1b4546c2bd40ba3ae5b1
Thomas Crowe <wcrowe@g.clemson.edu>
2022-07-16 14:04:11 -0400

    Added images and bootstrap grid to index page.

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,17 +4,25 @@ const Def = require('../default')
 const index = (data) => {
     let placesFormatted = data.places.map((place) => {
         return(
-            <div>
-                <h2>{place.name}</h2>
+            <div className="col-sm-6">
+                <h2 className="text-center">{place.name}</h2>
+                <p className="text-center">
+                    {place.cuisines}
+                </p>
                 <img src={place.pic} alt={place.name}></img>
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
             )
     })
     return(
         <Def>
             <main>
-                <h1>Places Index</h1>
-                {placesFormatted}
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
+                    {placesFormatted}
+                </div>
             </main>
         </Def>
         )
