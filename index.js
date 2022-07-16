e1a5f5d5e59efdc76bcb4ff7be22ebf76ab952d8
Thomas Crowe <wcrowe@g.clemson.edu>
2022-07-15 23:04:23 -0400

    Added styles

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 
+//Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -16,4 +19,5 @@ app.get('*', (req, res) => {
     res.status(404).render('error404')
 })
 
+//Listen for Connections 
 app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,9 @@ const home = () => {
         <Def>
             <main>
                 <h1>Home</h1>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
             </main>
         </Def>
         )
