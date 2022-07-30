aff35c37604ed76b187811b97f0bd21cd1bd294b
Thomas Crowe <wcrowe@g.clemson.edu>
2022-07-30 10:20:37 -0400

    Added links to footer

--- views/default.jsx
+++ views/default.jsx
@@ -10,6 +10,7 @@ const Def = (html) => {
                 <title>Rest-rant Review App</title>*/}
                 <link rel="icon" type="image/png" href="/images/cutlery.png"></link>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                 <link rel="stylesheet" href="/css/styles.css"></link>
             </head>
             <body>
@@ -38,8 +39,12 @@ const Def = (html) => {
                     <a id="favLink" href="https://www.flaticon.com/free-icons/restaurant" title="restaurant icons">Restaurant icons created by Freepik - Flaticon</a>
                     {html.children}
                 </div>
-                <footer className="footer-footer fixed-bottom">
-                    My Links go here.
+                <footer className="footer-footer fixed-bottom d-flex justify-content-center">
+                    <a title="GitHub" href="https://github.com/wtcrowe4" className="fa fa-github social"></a>
+                    <a title="LinkedIn" href="https://www.linkedin.com/in/thomas-crowe-0a8313167" className="fa fa-linkedin social"></a>
+                    <a title="Instagram" href="https://www.instagram.com/tcroweiv" className="fa fa-instagram social"></a>
+                    <a title="Facebook" href="https://www.facebook.com/tcrowe4" className="fa fa-facebook social"></a>
+                    <a title="Email" href="mailto:wcrowe@g.clemson.edu" className="fa fa-envelope social"></a>
                 </footer>
             </body>
         </html>
